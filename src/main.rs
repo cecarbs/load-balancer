9d2155a54f8603af871b4822a5e0c761a5efe0bd
Charles Carbonel <carbonel257@gmail.com>
2023-12-03 19:22:06 -0600

    Removed async code and fixed bug; load balancer working

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,7 @@ use std::{
     sync::{Arc, Mutex, MutexGuard},
 };
 
-use load_balancer::{RingBuffer, Routes, ThreadPool};
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-
-    // Reads and prints the request from the client
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    // Print out the request from the client
-    println!("Request: {:?}", http_request);
-
-    let response = "HTTP/1.1 200 OK\r\n\r\nReplied with a hello message";
-
-    // TODO: error handle here
-    stream.write_all(response.as_bytes()).unwrap();
-}
-
-// #[tokio::main]
-// async fn main() {
-//     let pool = ThreadPool::new(4);
-//     let listener: TcpListener =
-//         TcpListener::bind("127.0.0.1:8080").expect("Failed to bind, port already in use ");
-//
-//     let mut routes = Routes::new(2);
-//     if routes.add_server("http://127.0.0.1:8081").is_ok() {
-//         println!("Successfully added server!");
-//     } else {
-//         println!("Something went wrong!");
-//     }
-//
-//     for stream in listener.incoming() {
-//         pool.execute(move || match stream {
-//             Ok(stream) => {
-//                 // handle_connection(stream);
-//                 async_get(stream, "http://127.0.0.1:8082");
-//             }
-//             Err(e) => {
-//                 println!("Connection failed with error: {}", e);
-//             }
-//         })
-//     }
-// }
+use load_balancer::{Routes, ThreadPool};
 
 fn main() {
     let pool = ThreadPool::new(4);
@@ -69,13 +24,9 @@ fn main() {
         routes.add_server("http://127.0.0.1.8081").unwrap();
     } // Lock is automaically relased when "routes" goes out of scope
 
-    let mut count = 0;
     for stream in listener.incoming() {
-        // count += 1;
-        // println!("The current count is: {:?}", count);
         let arc_routes = Arc::clone(&routes);
         pool.execute(move || match stream {
-            // Declare a variable to hold the server path
             Ok(stream) => {
                 let server: String;
                 // Lock scope
@@ -85,6 +36,7 @@ fn main() {
 
                     server = routes.get_server().to_string();
                 }
+                println!("Current route is :{:?}", server);
                 // handle_connection(stream);
                 blocking_get(stream, &server).unwrap();
             }
@@ -106,31 +58,11 @@ fn blocking_get(mut stream: TcpStream, route: &str) -> Result<(), Box<dyn std::e
 
     println!("Request: {:?}", http_request);
 
-    let response_from_server = reqwest::blocking::get(route)?.text()?;
+    let body = reqwest::blocking::get(route)?.text()?;
 
-    let response = String::from("HTTP/1.1 200 OK\r\n\r\n") + response_from_server.as_str();
+    let response = String::from("HTTP/1.1 200 OK\r\n\r\n") + body.as_str();
 
     stream.write_all(response.as_bytes()).unwrap();
 
     Ok(())
 }
-
-// async fn async_get(mut stream: TcpStream, route: &str) -> Result<(), Box<dyn std::error::Error>> {
-//     let buf_reader = BufReader::new(&mut stream);
-//
-//     let http_request: Vec<_> = buf_reader
-//         .lines()
-//         .map(|result| result.unwrap())
-//         .take_while(|line| !line.is_empty())
-//         .collect();
-//
-//     println!("Request: {:?}", http_request);
-//
-//     let response_from_server = reqwest::get(route).await?.text().await?;
-//
-//     let response = String::from("HTTP/1.1 200 OK\r\n\r\n") + response_from_server.as_str();
-//
-//     stream.write_all(response.as_bytes()).unwrap();
-//
-//     Ok(())
-// }
